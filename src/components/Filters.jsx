eb793648129690a291c28b57b1f2ca9501c985af
lebarrichello <leticia.barrichello@gmail.com>
2023-07-05 15:29:21 -0300

    :sparkles: Criado filtro numerico com tres seletores

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,31 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useCallback, useReducer } from 'react';
 import Context from '../context/Context';
 
 function Filters() {
-  const { filters: { filterByName }, setFilterByName } = useContext(Context);
+  const { filters: { filterByName }, setFilterByName,
+    addNumericFilter, removeNumericFilter, columnOpt } = useContext(Context);
 
   const handleChange = useCallback(({ target: { value } }) => {
     setFilterByName(value);
   }, [setFilterByName]);
+
+  const [numFilters, setNumericFilter] = useReducer((state, newState) => ({
+    ...state, ...newState,
+  }), {
+    column: 'population',
+    comparison: 'maior que',
+    value: '0',
+  });
+
+  const addNumFilterContext = useCallback(() => {
+    addNumericFilter(numFilters);
+  }, [addNumericFilter, numFilters]);
+
+  const handleChangeNumFilter = useCallback(({ target: { name, value } }) => {
+    setNumericFilter({ [name]: value });
+  }, []);
+
+  const removeAll = useCallback(() => removeNumericFilter('All'), [removeNumericFilter]);
 
   return (
     <div className="containerFilters">
@@ -18,6 +37,53 @@ function Filters() {
           onChange={ handleChange }
           value={ filterByName }
         />
+      </div>
+      <div className="filterNumcont">
+        <select
+          label="Coluna"
+          data-testid="column-filter"
+          name="column"
+          onChange={ handleChangeNumFilter }
+          value={ numFilters.column }
+        >
+          {columnOpt.map((option) => (
+            <option key={ option } value={ option }>{option}</option>
+          ))}
+        </select>
+
+        <select
+          label="Operador"
+          data-testid="comparison-filter"
+          name="comparison"
+          onChange={ handleChangeNumFilter }
+          value={ numFilters.comparison }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+
+        <input
+          name="value"
+          type="number"
+          data-testid="value-filter"
+          onChange={ handleChangeNumFilter }
+          value={ numFilters.value }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ addNumFilterContext }
+        >
+          Filtrar
+        </button>
+        <button
+          type="button"
+          data-testid="button-remove-filters"
+          onClick={ removeAll }
+        >
+          Remover Filtros
+        </button>
       </div>
     </div>
   );
